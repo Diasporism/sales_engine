08239a3124673985051d2444325950992c086972
Chelsea Komlo <chelsea.komlo@gmail.com>
2013-02-14 13:50:58 -0700

    Invoice Item Debugged

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -2,7 +2,7 @@ class InvoiceItem
   attr_reader :id, :item_id, :invoice_id, :quantity, :unit_price, :created_at, :updated_at
 
   def initialize(row)
-    @id = row[:id]
+    @id = row[:id].to_i
     @item_id = row[:item_id].to_i
     @invoice_id = row[:invoice_id].to_i
     @quantity = row[:quantity].to_i
