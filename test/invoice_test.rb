5044f71faac4aebc2cf703a7d738e83a176a309e
Chelsea Komlo <chelsea.komlo@gmail.com>
2013-02-13 20:25:17 -0700

    Invoice Test Updated

--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -10,15 +10,114 @@ class InvoiceTest < MiniTest::Unit::TestCase
   end
 
   def test_invoices_have_correct_state
-    data = {:id => '1', :customer_id => '2', :merchant_id => '3', :status => 'shipped',
+    data = {:id => '1', :invoice_id => '2', :merchant_id => '3', :status => 'shipped',
             :created_at => '2012-03-27 14:53:59 UTC', :updated_at => '2012-03-27 14:53:59 UTC'}
 
     invoice = Invoice.new(data)
     assert_equal '1', invoice.id
-    assert_equal '2', invoice.customer_id
+    assert_equal '2', invoice.invoice_id
     assert_equal '3', invoice.merchant_id
     assert_equal 'shipped', invoice.status
     assert_equal '2012-03-27 14:53:59 UTC', invoice.created_at
     assert_equal '2012-03-27 14:53:59 UTC', invoice.updated_at
   end
+
+  def test_it_returns_random_invoice
+    invoice_one = invoice.random
+    invoice_two = invoice.random
+    invoice_three = invoice.random
+    invoice_four = invoice.random
+    assert invoice_one != nil
+    assert invoice_two != nil
+    assert invoice_three != nil
+    assert invoice_one != invoice_two || invoice_one != invoice_three || invoice_one != invoice_four
+  end
+
+  id,customer_id,merchant_id,status,created_at,updated_at
+1,1,26,shipped,2012-03-25 09:54:09 UTC,2012-03-25 09:54:09 UTC
+
+  ############################ ID
+
+  def test_it_finds_invoices_by_id
+    invoice = Invoice.find_by_id(8)
+    assert invoice != nil
+    assert invoice.id == 8
+  end
+
+  def test_it_finds_all_invoices_by_id
+    invoices = Invoice.find_all_by_id(8)
+    assert invoices.count == 1
+    assert invoices.each { |invoice| invoice.id == 8 }
+  end
+
+  ############################ Customer_ID
+
+  def test_it_finds_invoices_by_customer_id
+    invoice = Invoice.find_by_customer_id(1)
+    assert invoice != nil
+    assert_equal invoice.customer_id, 1
+  end
+
+  def test_it_finds_all_invoices_by_customer_id
+    invoices = Invoice.find_all_by_customer_id('Joey')
+    assert invoices.count == 2
+    assert invoices.each { |invoice| invoice.customer_id == 'Joey' }
+  end
+
+  ############################ Merchant_ID
+
+  def test_it_finds_invoices_by_merchant_id
+    invoice = Invoice.find_by_merchant_id(26)
+    assert invoice != nil
+    assert invoice.merchant_id == 26
+  end
+
+  def test_it_finds_all_invoices_by_merchant_id
+    invoices = Invoice.find_all_by_merchant_id(26)
+    assert invoices.count == 2
+    assert invoices.each { |invoice| invoice.merchant_id == 26 }
+  end
+
+    ############################ Status
+
+  def test_it_finds_invoices_by_status
+    invoice = Invoice.find_by_status('shipped')
+    assert invoice != nil
+    assert invoice.status == 'shipped'
+  end
+
+  def test_it_finds_all_invoices_by_status
+    invoices = Invoice.find_all_by_status('shipped')
+    assert invoices.count == 10
+    assert invoices.each { |invoice| invoice.status == 'shipped' }
+  end
+
+  ############################ Created_At
+
+  def test_it_finds_invoices_by_created_at
+    invoice = Invoice.find_by_created_at('2012-03-25 09:54:09 UTC')
+    assert invoice != nil
+    assert invoice.created_at == '2012-03-25 09:54:09 UTC'
+  end
+
+  def test_it_finds_all_invoices_by_created_at
+    invoices = Invoice.find_all_by_created_at('2012-03-25 09:54:09 UTC')
+    assert invoices.count == 2
+    assert invoices.each { |invoice| invoice.created_at == '2012-03-25 09:54:09 UTC' }
+  end
+
+  ############################ Updated_At
+
+  def test_it_finds_invoices_by_updated_at
+    invoice = Invoice.find_by_updated_at('2012-03-25 09:54:09 UTC')
+    assert invoice != nil
+    assert invoice.updated_at == '2012-03-25 09:54:09 UTC'
+  end
+
+  def test_it_finds_all_invoices_by_updated_at
+    invoices = Invoice.find_all_by_updated_at('2012-03-25 09:54:09 UTC')
+    assert invoices.count == 2
+    assert invoices.each { |invoice| invoice.updated_at == '2012-03-25 09:54:09 UTC' }
+  end
+
 end
